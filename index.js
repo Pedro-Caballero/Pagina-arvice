87b773260985fda48b0a440848eeb160c56183a2
Pc2015600258 <pedro-caba110@outlook.com>
2020-06-16 20:31:00 -0500

    jojo

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const routes = require('./route/index');
 
 server.use('/', routes());
 
-var port = process.env.port || 8080;
+// var port = process.env.port || 8080;
 
-server.listen(port, () => {
+server.listen('8080', () => {
     console.log('Servidor corriendo en el puerto: 8080');
 })
